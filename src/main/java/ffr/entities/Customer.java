72252e3f1b93e388a8f090730ca8367c4f730b33
eole9 <eole9@yahoo.com>
2015-04-25 21:53:08 +0200

    toString 2

--- src/main/java/ffr/entities/Customer.java
+++ src/main/java/ffr/entities/Customer.java
@@ -49,7 +49,11 @@ public class Customer {
 
     @Override
     public String toString() {
-        return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 
 }
