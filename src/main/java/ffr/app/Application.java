7350f9049f32d13aa300afcb7688837b93d68bfd
MC <MC@PC-de-MC>
2016-05-15 18:58:22 +0200

    V4

--- src/main/java/ffr/app/Application.java
+++ src/main/java/ffr/app/Application.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-// FFR v3
+// FFR v4
 
 @Configuration
 @EnableAutoConfiguration(exclude = { WebInitializer.class })
