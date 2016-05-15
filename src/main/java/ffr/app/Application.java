31c9a47274b29a5d873decbd626a4d85f307cba1
MC <MC@PC-de-MC>
2016-05-15 17:35:58 +0200

    TEST FFR

--- src/main/java/ffr/app/Application.java
+++ src/main/java/ffr/app/Application.java
@@ -11,18 +11,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+// FFR
+
 @Configuration
-@EnableAutoConfiguration(exclude = {WebInitializer.class})
-@ComponentScan(basePackages = {"ffr.app", "ffr.services", "ffr.controllers", "ffr.security"})
-@EnableJpaRepositories(basePackages = {"ffr.repositories"})
-@EntityScan(basePackages = {"ffr.entities"})
+@EnableAutoConfiguration(exclude = { WebInitializer.class })
+@ComponentScan(basePackages = { "ffr.app", "ffr.services", "ffr.controllers", "ffr.security" })
+@EnableJpaRepositories(basePackages = { "ffr.repositories" })
+@EntityScan(basePackages = { "ffr.entities" })
 @EnableTransactionManagement
 public class Application {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class);
 
-        CustomerRepository repository = context.getBean(CustomerRepository.class);
-        repository.save(new Customer("Jack", "Bauer"));
-    }
+		CustomerRepository repository = context.getBean(CustomerRepository.class);
+		repository.save(new Customer("Jack", "Bauer"));
+	}
 }
